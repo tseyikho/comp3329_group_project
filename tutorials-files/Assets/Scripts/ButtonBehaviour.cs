8a682f84afa5ed05832b6ac26c1e6d1e042548fd
UchihaDZodlyck <UchihaDZodlyck@users.noreply.github.com>
2020-04-13 22:31:57 +0800

    Add youtube tutorial files

---
+++ tutorials-files/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonBehaviour : MonoBehaviour
+{
+   public enum ButtonId
+    {
+        roomChangeButton, returnButton
+    }
+
+    public ButtonId ThisButtionId;
+
+    private DisplayImage currentDisplay;
+
+    void Start()
+    {
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+    }
+
+    void Update()
+    {
+        HideDisplay();
+        Display();
+
+    }
+
+    void HideDisplay()
+    {
+        if(currentDisplay.CurrentState == DisplayImage.State.normal && ThisButtionId == ButtonId.returnButton)
+        {
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g,
+                GetComponent<Image>().color.b, 0);
+
+            GetComponent<Button>().enabled = false;
+
+            this.transform.SetSiblingIndex(0);
+        }
+
+        if (currentDisplay.CurrentState == DisplayImage.State.zoom && ThisButtionId == ButtonId.roomChangeButton)
+        {
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g,
+    GetComponent<Image>().color.b, 0);
+
+            GetComponent<Button>().enabled = false;
+
+            this.transform.SetSiblingIndex(0);
+        }
+    }
+    void Display()
+    {
+        if (currentDisplay.CurrentState == DisplayImage.State.zoom && ThisButtionId == ButtonId.returnButton)
+        {
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g,
+                GetComponent<Image>().color.b, 1);
+
+            GetComponent<Button>().enabled = true;
+        }
+
+        if (currentDisplay.CurrentState == DisplayImage.State.normal && ThisButtionId == ButtonId.roomChangeButton)
+        {
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g,
+                GetComponent<Image>().color.b, 1);
+
+            GetComponent<Button>().enabled = true;
+        }
+    }
+}
---
+++ tutorials-files/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private DisplayImage currentDisplay;
+
+    private float initialCameraSize;
+    private Vector3 initialCameraPosition;
+
+    
+
+
+    void Start()
+    {
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+        initialCameraSize = Camera.main.orthographicSize;
+        initialCameraPosition = Camera.main.transform.position;
+    }
+
+    public void OnRightClickArrow()
+    {
+        currentDisplay.CurrentWall = currentDisplay.CurrentWall + 1;
+    }
+
+    public void OnLeftClickArrow()
+    {
+        currentDisplay.CurrentWall = currentDisplay.CurrentWall - 1;
+    }
+
+    public void OnClickZoomReturn()
+    {
+        GameObject.Find("displayImage").GetComponent<DisplayImage>().CurrentState = DisplayImage.State.normal;
+        var zoomInOnjects = FindObjectsOfType<ZoomInObject>();
+        foreach(var zoomInObject in zoomInOnjects)
+        {
+            zoomInObject.gameObject.layer = 0;
+        }
+        Camera.main.orthographicSize = initialCameraSize;
+        Camera.main.transform.position = initialCameraPosition;
+
+    }
+}
